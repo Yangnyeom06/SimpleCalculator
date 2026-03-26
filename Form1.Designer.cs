7b9cd62a367878d00d132bc991d294528c1c160c
yangnyeom06 <lyujin1204@gmail.com>
2026-03-26 16:02:27 +0900

    과제1 코딩과 테스트 완료 기본 UI 배치 완료 숫자 입력 받아서 textResultBox와 textInputBox에 넣기 + 연산자 클릭 시 textResultBox에 추가 연산자가 이미 들어와있을 시 추가 입력 받지 않음 textResultBox에 모든 식을 넣어서 출력하고 textInputBox에는 지금 입력하는 숫자만 출력
    결과를 출력할때는 resultText에 있는 값을 계산한 뒤 = 결과값을 출력
    
    결과 값을 출력하고도 결과 값에 이어서 입력 가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            title = new Label();
+            textResultBox = new TextBox();
+            textInputBox = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            buttonAdd = new Button();
+            buttonCal = new Button();
+            SuspendLayout();
+            // 
+            // title
+            // 
+            title.Font = new Font("맑은 고딕", 20F);
+            title.Location = new Point(12, 9);
+            title.Name = "title";
+            title.Size = new Size(367, 60);
+            title.TabIndex = 0;
+            title.Text = "Simple Calculator";
+            // 
+            // textResultBox
+            // 
+            textResultBox.Font = new Font("맑은 고딕", 15F);
+            textResultBox.Location = new Point(23, 88);
+            textResultBox.Name = "textResultBox";
+            textResultBox.Size = new Size(329, 41);
+            textResultBox.TabIndex = 1;
+            // 
+            // textInputBox
+            // 
+            textInputBox.Font = new Font("맑은 고딕", 15F);
+            textInputBox.Location = new Point(23, 146);
+            textInputBox.Name = "textInputBox";
+            textInputBox.Size = new Size(329, 41);
+            textInputBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15F);
+            button1.Location = new Point(23, 345);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 47);
+            button1.TabIndex = 3;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 15F);
+            button2.Location = new Point(101, 345);
+            button2.Name = "button2";
+            button2.Size = new Size(72, 47);
+            button2.TabIndex = 4;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 15F);
+            button3.Location = new Point(179, 345);
+            button3.Name = "button3";
+            button3.Size = new Size(72, 47);
+            button3.TabIndex = 5;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("맑은 고딕", 15F);
+            button4.Location = new Point(23, 398);
+            button4.Name = "button4";
+            button4.Size = new Size(72, 47);
+            button4.TabIndex = 6;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("맑은 고딕", 15F);
+            button5.Location = new Point(101, 398);
+            button5.Name = "button5";
+            button5.Size = new Size(72, 47);
+            button5.TabIndex = 7;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("맑은 고딕", 15F);
+            button6.Location = new Point(179, 398);
+            button6.Name = "button6";
+            button6.Size = new Size(72, 47);
+            button6.TabIndex = 8;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("맑은 고딕", 15F);
+            button7.Location = new Point(23, 451);
+            button7.Name = "button7";
+            button7.Size = new Size(72, 47);
+            button7.TabIndex = 9;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("맑은 고딕", 15F);
+            button8.Location = new Point(101, 451);
+            button8.Name = "button8";
+            button8.Size = new Size(72, 47);
+            button8.TabIndex = 10;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("맑은 고딕", 15F);
+            button9.Location = new Point(179, 451);
+            button9.Name = "button9";
+            button9.Size = new Size(72, 47);
+            button9.TabIndex = 11;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("맑은 고딕", 15F);
+            button0.Location = new Point(101, 504);
+            button0.Name = "button0";
+            button0.Size = new Size(72, 47);
+            button0.TabIndex = 12;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button0_Click;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Font = new Font("맑은 고딕", 15F);
+            buttonAdd.Location = new Point(257, 292);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(72, 47);
+            buttonAdd.TabIndex = 13;
+            buttonAdd.Text = "+";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
+            // 
+            // buttonCal
+            // 
+            buttonCal.Font = new Font("맑은 고딕", 15F);
+            buttonCal.Location = new Point(257, 504);
+            buttonCal.Name = "buttonCal";
+            buttonCal.Size = new Size(72, 47);
+            buttonCal.TabIndex = 14;
+            buttonCal.Text = "=";
+            buttonCal.UseVisualStyleBackColor = true;
+            buttonCal.Click += buttonCal_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(374, 574);
+            Controls.Add(buttonCal);
+            Controls.Add(buttonAdd);
+            Controls.Add(button0);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textInputBox);
+            Controls.Add(textResultBox);
+            Controls.Add(title);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label title;
+        private TextBox textResultBox;
+        private TextBox textInputBox;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button buttonAdd;
+        private Button buttonCal;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,119 @@
+using System.Data;
+
 namespace SimpleCalculator
 {
     public partial class Form1 : Form
     {
+        string resultText = "";
+        string inputText = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonNumInput(string value)
+        {
+            inputText += $"{value}";
+            textInputBox.Text = inputText;
+
+            resultText += $"{value}";
+            textResultBox.Text = resultText;
+        }
+
+        private void buttonOprInput(string value)
+        {
+            resultText = resultText.TrimEnd();
+
+            if (resultText.Length == 0) return;
+
+            if ("+-*/".Contains(resultText.Last()))
+            {
+                resultText = resultText.Substring(0, resultText.Length - 1);
+            }
+
+            resultText += $" {value} ";
+            textResultBox.Text = resultText;
+
+            inputText = "";
+            textInputBox.Text = inputText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("9");
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            buttonNumInput("0");
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            buttonOprInput("+");
+        }
+
+        private void buttonCal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                var result = dt.Compute(resultText, "");
+
+                resultText += $" = {result.ToString()}";
+                textResultBox.Text = resultText;
+                textInputBox.Text = result.ToString();
+                resultText = result.ToString();
+                inputText = result.ToString();
+  
+            }
+            catch
+            {
+                textResultBox.Text = "Error";
+                resultText = "";
+  
+            }
         }
     }
 }
